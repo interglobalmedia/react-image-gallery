3d033c3b80886c7b13d3d9d2dfb7bd1a0f35cd57
interglobalmedia <interglobalmedia@gmail.com>
2017-11-07 07:07:45 -0500

    Added radium to project in Gallery.js and GalleryModal.js.

--- src/components/GalleryModal.js
+++ src/components/GalleryModal.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Radium from 'radium';
 
 class GalleryModal extends Component {
 	constructor() {
@@ -23,15 +24,51 @@ class GalleryModal extends Component {
 		}
 	}
 	render() {
+		const modalOverlayStyle = {
+		    position: 'fixed',
+		    zIndex: '1',
+		    height: '100%',
+		    width: '100%',
+		    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+		    top: '0',
+		    left: '0'
+		}
+		const modalStyle = {
+			position: 'fixed',
+		    zIndex: '999',
+		    width: '50%',
+		    maxWidth: '800px',
+		    top: '50%',
+		    left: '50%',
+		    transform: 'translate3d(-50%, -50%, 0)'
+		}
+		const modalBodyStyle = {
+			position: 'relative'
+		}
+		const mediaMin320StyleModal = {
+			'@media (min-width: 320px)': {
+				width: '95%'
+			}
+		}
+		const mediaMin544StyleModal = {
+			'@media (min-width: 544px)': {
+				width: '80%'
+			}
+		}
+		const mediaMin960StyleModal = {
+			'@media (min-width: 960px)': {
+				width: '60%'
+			}
+		}
 		const {closeModal, hasNext, hasPrev, findNext, findPrev, src} = this.props;
 		if(!src) {
 			return null;
 		}
 		return (
 			<div>
-				<div className='modal-overlay' onClick={closeModal}></div>
-				<div className='modal' isOpen={!!src}>
-					<div className='modal-body'>
+				<div className='modal-overlay' style={modalOverlayStyle} onClick={closeModal}></div>
+				<div className='modal' style={[modalStyle, mediaMin320StyleModal, mediaMin544StyleModal, mediaMin960StyleModal]} isOpen={!!src}>
+					<div className='modal-body' style={modalBodyStyle}>
 						<a href='#' className='modal-close' onClick={closeModal} onKeyDown={this.handleKeyDown}>&times;</a>
 						{hasPrev && <a href='#' className='modal-prev' onClick={findPrev} onKeyDown={this.handleKeyDown}>&lsaquo;</a>}
 						{hasNext && <a href='#' className='modal-next' onClick={findNext} onKeyDown={this.handleKeyDown}>&rsaquo;</a>}
@@ -43,4 +80,4 @@ class GalleryModal extends Component {
 	}
 }
 
-export default GalleryModal;
+export default Radium(GalleryModal);
